c3448ec0e1a7b734546955b85e1a59735a7cf8fc
Suchandan <suchandansau2005@gmail.com>
2026-08-22 16:13:26 +0530

    Initial project setup

---
+++ test_environment.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import xgboost
+import tensorflow as tf
+import shap
+import fastapi
+import streamlit
+import plotly
+import mysql.connector
+import sqlalchemy
+
+print("Pandas:", pd.__version__)
+print("NumPy:", np.__version__)
+print("Scikit-learn:", sklearn.__version__)
+print("XGBoost:", xgboost.__version__)
+print("TensorFlow:", tf.__version__)
+print("SHAP:", shap.__version__)
+print("FastAPI:", fastapi.__version__)
+print("Streamlit:", streamlit.__version__)
+print("Plotly:", plotly.__version__)
+print("SQLAlchemy:", sqlalchemy.__version__)
+
+print("\nAll project libraries are working!")
