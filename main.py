ce1d4024f11048ad6c71285165a28fb02a3480f7
Suchandan <suchandansau2005@gmail.com>
2026-08-25 22:26:32 +0530

    Fix CORS for production frontend

--- main.py
+++ main.py
@@ -33,7 +33,8 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://127.0.0.1:8000",
-        "http://localhost:8000"
+        "http://localhost:8000",
+        "https://fraudshield-frontend-bqor.onrender.com"
     ],
 
     allow_credentials=True,
